c7065f81748905bb88277cc878ccf617a7c312bc
oksesaneka22 <49028843+oksesaneka22@users.noreply.github.com>
2025-02-02 16:38:33 +0200

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# MySQL credentials
+MYSQL_USER="root"
+MYSQL_PASSWORD="password"
+MYSQL_HOST="localhost"
+MYSQL_DB="todo_db"
+
+# Backup directory
+BACKUP_DIR="/backups"
+DATE=$(date +"%Y-%m-%d_%H-%M-%S")
+BACKUP_FILE="${BACKUP_DIR}/todo_db_${DATE}.sql"
+
+# S3 Bucket
+S3_BUCKET="s3://backeups-site123/backups"
+
+# Create backup
+echo "Backing up MySQL database..."
+mysqldump -u $MYSQL_USER -p$MYSQL_PASSWORD -h $MYSQL_HOST $MYSQL_DB > $BACKUP_FILE
+
+# Upload to S3
+echo "Uploading backup to S3..."
+aws s3 cp $BACKUP_FILE $S3_BUCKET/
+
+# Optionally, you can delete the local backup after upload
+# rm $BACKUP_FILE
+
+echo "Backup completed!"
