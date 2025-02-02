6bef4a1ab1d7c289c215ad0f72c007c095652254
oksesaneka22 <49028843+oksesaneka22@users.noreply.github.com>
2025-02-02 16:46:45 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -7,16 +7,19 @@ MYSQL_HOST="localhost"
 MYSQL_DB="todo_db"
 
 # Backup directory
-BACKUP_DIR="/backups"
+BACKUP_DIR="/tmp/backups"
 DATE=$(date +"%Y-%m-%d_%H-%M-%S")
 BACKUP_FILE="${BACKUP_DIR}/todo_db_${DATE}.sql"
 
-# S3 Bucket
-S3_BUCKET="s3://backeups-site123/backups"
+# Create backup directory if it doesn't exist
+mkdir -p $BACKUP_DIR
 
 # Create backup
 echo "Backing up MySQL database..."
 mysqldump -u $MYSQL_USER -p$MYSQL_PASSWORD -h $MYSQL_HOST $MYSQL_DB > $BACKUP_FILE
+
+# S3 Bucket
+S3_BUCKET="s3://backeups-site123/backups"
 
 # Upload to S3
 echo "Uploading backup to S3..."
